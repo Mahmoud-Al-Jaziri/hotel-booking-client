9a0225e449d5e54e5a653452bee0d9b5939ae8e3
Mahmoud-Al-Jaziri <119842984+Mahmoud-Al-Jaziri@users.noreply.github.com>
2026-04-09 21:22:29 +0800

    update API URL to live

--- src/api/bookings.js
+++ src/api/bookings.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000"
+const API_URL = "https://hotel-booking-api-psi.vercel.app"
 
 export const getBookings = ()=>{
     return axios.get(`${API_URL}/bookings`)
